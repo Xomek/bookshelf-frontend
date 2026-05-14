0dd736018a045445aef62aaf48bef41f0f7426cf
Xomek <z_v_m_x@mail.ru>
2026-05-14 20:02:21 +0700

    feature: Изменил Header для страницы Добавления новой книги и добавил стрелочку назад

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { ROUTES } from '@shared/constants/routes';
 import { Button } from '@shared/ui/Button';
-import { Book, Plus } from 'lucide-react';
+import { Book, Plus, ArrowLeft } from 'lucide-react';
 import { useLocation, useNavigate } from 'react-router';
 
 export const Header = () => {
@@ -12,11 +12,25 @@ export const Header = () => {
   return (
     <header className="flex h-25 items-center border-b border-zinc-200 mb-8">
       <div className="mx-auto flex w-full max-w-350 items-center justify-between px-4">
-        <div className="flex items-center gap-2">
-          <Book size={40} />
+        <div className="flex items-center gap-4">
+          {isRootPage ? (
+            <Book size={40} />
+          ) : (
+            <ArrowLeft
+              className="cursor-pointer w-10"
+              onClick={() => navigate(-1)}
+            />
+          )}
+
           <div>
-            <h1 className="text-2xl">Моя библиотека</h1>
-            <span className="text-sm text-zinc-500">6 книг в коллекции</span>
+            <h1 className="text-2xl">
+              {isRootPage ? 'Моя библиотека' : 'Добавить книгу'}
+            </h1>
+            <span className="text-sm text-zinc-500">
+              {isRootPage
+                ? '6 книг в коллекции'
+                : 'Создайте новую запись в вашей библиотеке'}
+            </span>
           </div>
         </div>
 
