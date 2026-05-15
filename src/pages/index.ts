f72f39679d2cd17117caac7a4e7a84748f3b831f
Xomek <z_v_m_x@mail.ru>
2026-05-15 17:54:12 +0700

    feature: Добавил компонент ProgressBar

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { BooksPage } from './BooksPage';
 export { AddNewBookPage } from './AddNewBookPage';
 export { SettingsPage } from './SettingsPage';
+export { ReaderPage } from './ReaderPage';
---
+++ src/shared/ui/ProgressBar.tsx
@@ -0,0 +1,11 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export const ProgressBar = ({ progress }: ProgressBarProps) => {
+  return (
+    <div className="w-full bg-zinc-200 dark:bg-zinc-700 rounded-full h-2 overflow-hidden">
+      <div className="bg-primary h-full" style={{ width: `${progress}%` }} />
+    </div>
+  );
+};
