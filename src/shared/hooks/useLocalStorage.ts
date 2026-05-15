71c6b15291e0fb126bb03c1d5348483c475f7fba
Xomek <z_v_m_x@mail.ru>
2026-05-15 19:41:21 +0700

    feature: Добавил shared хуки useClickOutside и useLocalStorage

---
+++ src/shared/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useCallback, useState } from 'react';
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  });
+
+  const setValue = useCallback(
+    (value: T | ((prev: T) => T)) => {
+      setStoredValue((prev) => {
+        const nextValue = value instanceof Function ? value(prev) : value;
+        localStorage.setItem(key, JSON.stringify(nextValue));
+        return nextValue;
+      });
+    },
+    [key],
+  );
+
+  return [storedValue, setValue] as const;
+};
